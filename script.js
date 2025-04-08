48111c89efe5f2afa697aceca0981616bccb7170
MithunKumarRajak <mithunkumarrajak18plus@gmail.com>
2025-04-09 00:43:24 +0530

    After adding the scroll up button

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showSection(sectionId) {
     if (activeSection) {
         activeSection.classList.add('active');
     }
-} 
+}
 
 // Dark mode 
 window.onload = function () {
@@ -35,3 +35,67 @@ window.onload = function () {
         }
     };
 };
+
+// Google form submit
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwcGAZiE6cAoynmBaPoqPedCFTf1H2PjL5AFymY4tx8oCYRABjnoCcR1FdXpu18lhXh/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully";
+            msg.style.color = "#24b300"
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 5000)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+// side bar menu for small device
+
+var sidemenu = document.querySelector("nav ul");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// Tab link
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// scroll up button
+const scrollUpButton = document.getElementById('scrollUp');
+
+// Show the button when the user scrolls down 200px
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollUpButton.style.display = 'block';
+    } else {
+        scrollUpButton.style.display = 'none';
+    }
+});
+
+// Scroll to the top when the button is clicked
+scrollUpButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
